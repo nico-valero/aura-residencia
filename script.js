4ad48dbb25c41a57271dc3abf0b8774968dc9c77
nico-valero <56361794+nico-valero@users.noreply.github.com>
2025-03-19 21:23:06 -0300

    Carousel snap fix

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ const carousel = document.querySelector(".carousel"),
 firstImg = carousel.querySelectorAll("img")[0];
 const arrowIcons = document.querySelectorAll(".carousel-wrapper i");
 
-let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft;
 
 const showHideIcons = () => {
     let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
@@ -111,19 +111,6 @@ arrowIcons.forEach(icon => {
     });
 });
 
-const autoSlide = () => {
-    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-
-    positionDiff = Math.abs(positionDiff);
-    let firstImgWidth = firstImg.clientWidth + 14;
-    let valDifference = firstImgWidth - positionDiff;
-
-    if(carousel.scrollLeft > prevScrollLeft) {
-        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-    }
-    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-}
-
 const dragStart = (e) => {
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
@@ -135,18 +122,16 @@ const dragging = (e) => {
     e.preventDefault();
     isDragging = true;
     carousel.classList.add("dragging");
-    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    let currentX = e.pageX || e.touches[0].pageX;
+    let diff = currentX - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - diff;
     showHideIcons();
 }
 
 const dragStop = () => {
     isDragStart = false;
     carousel.classList.remove("dragging");
-
-    if(!isDragging) return;
     isDragging = false;
-    autoSlide();
 }
 
 carousel.addEventListener("mousedown", dragStart);
